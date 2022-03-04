ab0c9c005351cfa139aada48adb4a98f489ab332
edwarhman <edwarhman@gmail.com>
2022-03-04 19:19:57 -0400

    TASK-4: add createNewOffer function fix syntax errors

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,15 +1,17 @@
 pragma solidity >= 0.8.0 < 0.9.0;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Marketplace {
-	struct offer {
+	struct Offer {
 		address tokenAddress;
 		uint tokenId;
 		uint tokenAmount;
 		uint deadline;
 		uint price;
+		address seller;
 	}
 
-	offer[] offers;
+	Offer[] offers;
 	uint fee;
 
 	//events
@@ -31,7 +33,7 @@ contract Marketplace {
 		uint tokenAmount,
 		uint price,
 		address seller
-	)
+	);
 
 	event buyed (
 		uint offerId,
@@ -41,10 +43,46 @@ contract Marketplace {
 		uint price,
 		address seller,
 		address buyer
-	)
-	
+	);
+
 	function setFee(uint _fee) public {
 		fee = _fee;
 	}
+
+	function createNewOffer(
+		address tokenAddress,
+		uint tokenId,
+		uint tokenAmount,
+		uint deadline,
+		uint price
+	) 
+	public {
+		ERC1155 token = ERC1155(tokenAddress);
+		require(
+			token.balanceOf(msg.sender, tokenId) >= tokenAmount,
+			"You do not have enough tokens to create the offer"
+		);
+
+		Offer memory offer = Offer(
+			tokenAddress,
+			tokenId,
+			tokenAmount,
+			deadline + block.timestamp,
+			price,
+			msg.sender
+		);
+		offers.push(offer);
+
+		emit newOffer(
+			offers.length - 1,
+			token.uri(tokenId),
+		 	tokenId,
+			tokenAmount,
+			deadline,
+			price,
+			msg.sender
+		);
+
+	} 
 
 }
