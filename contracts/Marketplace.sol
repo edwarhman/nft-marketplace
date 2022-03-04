61b7dec5238f989f7aad7079052fddba9efe5eb8
edwarhman <edwarhman@gmail.com>
2022-03-04 15:29:00 -0400

    TASK-2: add events to contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -12,7 +12,39 @@ contract Marketplace {
 	offer[] offers;
 	uint fee;
 
+	//events
+
+	event newOffer(
+		uint offerId,
+		string tokenURI,
+		uint tokenId,
+		uint tokenAmount,
+		uint deadline,
+		uint price,
+		address seller
+	);
+
+	event canceledOffer(
+		uint offerId,
+		string tokenURI,
+		uint tokenId,
+		uint tokenAmount,
+		uint price,
+		address seller
+	)
+
+	event buyed (
+		uint offerId,
+		string tokenURI,
+		uint tokenId,
+		uint tokenAmount,
+		uint price,
+		address seller,
+		address buyer
+	)
+	
 	function setFee(uint _fee) public {
 		fee = _fee;
 	}
+
 }
