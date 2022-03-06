d86971496f8973da0cba77a104a5f040a203cdd9
edwarhman <edwarhman@gmail.com>
2022-03-06 13:07:38 -0400

    TASK-10: override uri() function

--- contracts/ERC1155Token.sol
+++ contracts/ERC1155Token.sol
@@ -1,9 +1,11 @@
 pragma solidity >=0.8.0 < 0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ERC1155Token is ERC1155 {
-	string name;
-	string symbol;
+	string public name;
+	string public symbol;
+	string baseExtension = ".json";
 
 	constructor(
 		string memory _name,
@@ -11,10 +13,16 @@ contract ERC1155Token is ERC1155 {
 		string memory _uri
 	) ERC1155(_uri) {
 		name = _name;
-		symbol = symbol;
+		symbol = _symbol;
 	}
 
 	function mint(address _to, uint _id, uint _amount) external {
     	_mint(_to, _id, _amount, "");
+ 	}
+
+ 	function uri(uint tokenId) public view override returns(string memory) {
+ 		return bytes(super.uri(tokenId)).length > 0
+        ? string(abi.encodePacked(super.uri(tokenId), Strings.toString(tokenId), baseExtension))
+        : "";
  	}
 }
