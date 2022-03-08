3ef3abe8a14faa1392747d3a8cc58949f54009a7
edwarhman <edwarhman@gmail.com>
2022-03-08 12:38:20 -0400

    TASK-29: add aggregator interfaces for currencies and functions to get their data

--- contracts/MarketplaceCurrencies.sol
+++ contracts/MarketplaceCurrencies.sol
@@ -1,15 +1,26 @@
 pragma solidity >= 0.8.0 < 0.9.0;
+
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/Denominations.sol";
 
 contract MarketplaceCurrencies is Initializable, OwnableUpgradeable {
-	address public daiAddress;
-	address public linkAddress;
+	AggregatorV3Interface internal ethPriceFeed;
+	AggregatorV3Interface internal daiPriceFeed;
+	AggregatorV3Interface internal linkPriceFeed;
 
-	function initialize(address _daiAddress, address _linkAddress) public initializer {
+	function initialize(
+		address _priceFeed,
+		address _daiPriceFeed,
+		address _linkPriceFeed
+	)
+	public
+	initializer {
 		__Ownable_init();
-		daiAddress = _daiAddress;
-		linkAddress = _linkAddress;
+		ethPriceFeed = AggregatorV3Interface(_priceFeed);
+		daiPriceFeed = AggregatorV3Interface(_daiPriceFeed);
+		linkPriceFeed = AggregatorV3Interface(_linkPriceFeed);
 	}
 
 	function _getPrice(
@@ -40,4 +51,50 @@ contract MarketplaceCurrencies is Initializable, OwnableUpgradeable {
 	internal {
 
 	}
+
+	function getEthUsdPrice() 
+	public
+	view
+	returns(int) {
+		(,int price,,,) = ethPriceFeed.latestRoundData();
+		return price;
+	}
+
+	function getDaiUsdPrice() 
+	public
+	view
+	returns(int) {
+		(,int price,,,) = daiPriceFeed.latestRoundData();
+		return price;
+	}
+
+	function getLinkUsdPrice() 
+	public
+	view
+	returns(int) {
+		(,int price,,,) = linkPriceFeed.latestRoundData();
+		return price;
+	}
+
+	function getEthPriceFeed()
+	public
+	view
+	returns(AggregatorV3Interface) {
+		return ethPriceFeed;
+	}
+
+	function getDaiPriceFeed()
+	public
+	view
+	returns(AggregatorV3Interface) {
+		return daiPriceFeed;
+	}
+
+	function getLinkPriceFeed()
+	public
+	view
+	returns(AggregatorV3Interface) {
+		return linkPriceFeed;
+	}
+	
 }
