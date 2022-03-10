5a2b48938b453e8035ba01f4e7f5bbb488551ce7
edwarhman <edwarhman@gmail.com>
2022-03-09 21:22:50 -0400

    TASK-56: add test cases for only ADMIN functions

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -64,7 +64,7 @@ describe("Marketplace contract", ()=> {
 		let tokenId = 1;
 		let tokenAmount = 20;
 		let price = 1000;
-
+		const ethEnum = 0;
 		let emptyOffer = [
 					ethers.constants.AddressZero,
 					ethers.BigNumber.from(0),
@@ -73,12 +73,16 @@ describe("Marketplace contract", ()=> {
 					ethers.constants.AddressZero
 				];
 
-		it("set selling fee", async ()=> {
-			await market.setFee(20);
-			expect(await market.fee())
-			.to
-			.equal(20);
-		})
+		beforeEach(async ()=> {
+			let tx = await token.mint(owner.address, tokenId, tokenAmount);
+			await market.createNewOffer(
+				token.address,
+				tokenId,
+				tokenAmount,
+				week,
+				price
+			);
+		});
 
 		describe("create new offer assumtions", ()=> {
 
@@ -90,17 +94,6 @@ describe("Marketplace contract", ()=> {
 					ethers.BigNumber.from(price),
 					owner.address
 				]; 
-
-				let tx = await token.mint(owner.address, tokenId, tokenAmount);
-				//await tx.wait();
-
-				await market.createNewOffer(
-					token.address,
-					tokenId,
-					tokenAmount,
-					week,
-					price
-				);
 
 				let offerData = (await market.offers(0)).slice(0, expectedData.length);
 				expect(offerData)
@@ -123,7 +116,7 @@ describe("Marketplace contract", ()=> {
 			});
 
 			it("Should not allow to create a new offer if seller have not the specified amount of tokens", async ()=>{
-				await expect(market.createNewOffer(
+				await expect(market.connect(addr1).createNewOffer(
 					token.address,
 					tokenId,
 					tokenAmount + 20,
@@ -137,20 +130,8 @@ describe("Marketplace contract", ()=> {
 		});
 
 		describe("Cancel offer assumtions", ()=> {
-			beforeEach(async ()=> {
-				await market.createNewOffer(
-					token.address,
-					tokenId,
-					tokenAmount,
-					week,
-					price
-				);
-			});
-
 			it("Should allow to cancel the offer", async ()=> {
 				
-
-
 				await market.cancelOffer(0);
 
 				let offerData = (await market.offers(0)).slice(0, emptyOffer.length);
@@ -168,17 +149,9 @@ describe("Marketplace contract", ()=> {
 			});
 		});
 
-		describe("Accept offer assumtions", ()=> {
-			const ethEnum = 0;
-			beforeEach(async ()=> {
-				await market.createNewOffer(
-					token.address,
-					tokenId,
-					tokenAmount,
-					week,
-					price
-				);
-			})
+		describe("AcceptOffer assumtions", ()=> {
+			
+			
 
 			it("Should not allow to accept an offer if sent payment is less than offer price", async ()=> {
 				await expect(market.connect(addr1).acceptOffer(0, ethEnum))
@@ -202,6 +175,50 @@ describe("Marketplace contract", ()=> {
 				.to
 				.deep
 				.equal(emptyOffer);
+			})
+		});
+
+		describe("Only Admin assumtions", ()=> {
+			it("Should not allow a non admin to call these functions", async ()=> {
+				await expect(market.connect(addr1).setFee(20))
+				.to
+				.be
+				.reverted;
+
+				await expect(market.connect(addr1).setRecipient(addr2.address))
+				.to
+				.be
+				.reverted;
+			});
+			it("Should set selling fee", async ()=> {
+				await market.setFee(20);
+				expect(await market.fee())
+				.to
+				.equal(20);
+			});
+
+			it("Should send funds to recipient", async()=> {
+				let prevBalance = await provider.getBalance(addr2.address);
+
+				await market.connect(addr1).acceptOffer(0, ethEnum, {value: weiPerEther});
+				await market.setRecipient(addr2.address);
+
+				await mockDaiCoin.transfer(market.address, weiPerEther.mul(100));
+				await mockLinkCoin.transfer(market.address, weiPerEther.mul(100));
+				await market.withdraw();
+				
+				expect(await provider.getBalance(owner.address))
+				.to
+				.above(prevBalance);
+
+				expect(await mockDaiCoin.balanceOf(addr2.address))
+				.to
+				.equal(weiPerEther.mul(100));
+
+				expect(await mockLinkCoin.balanceOf(addr2.address))
+				.to
+				.equal(weiPerEther.mul(100));
+
 			})
 		});
 	});
